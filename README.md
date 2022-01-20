d4ca049d97884e1b234f13b5df8e9c448aa6b894
Durru Aravind <aravind2000d@gmail.com>
2022-01-20 17:04:09 +0530

    Submitted

--- README.md
+++ README.md
@@ -6,11 +6,13 @@
 An easy to use 'Advanced Youtube Downloader' for downloading mp4/mp3 files from Youtube via GUI using Python libraries - Tkinter and Pytube.
 
 # Details:
-1. After installing python and pip, install the pre-requisite packages from pre-requisites.txt file.
+1. Execute the exe file - "ytb-downloader - Shortcut". If it does not execute go to step 2 or else skip step 2.
+
+2. Open the project in your code editor. After installing python and pip, install the pre-requisite packages from pre-requisites.txt file.
 
     Command: pip install -r pre-requisites.txt
 
-2. You can download either mp4 or mp3 or both. Downloaded files are saved in separate folders - Downloaded Videos and Downloaded Audios.
+2. You can download either mp4 or mp3 or both. Downloaded files are saved in folder paths (MP4 PATH AND MP3 PATH) given by the user.
 
 3. File - "history.txt" keeps record of the downloads and provides a link to download the video thumbnail image.
 
@@ -28,3 +30,5 @@ https://www.youtube.com/watch?v=3VejIihDfwU
 https://docs.python.org/3/tutorial/inputoutput.html
 
 https://www.youtube.com/watch?v=hLlDSC8NVGY
+
+https://www.geeksforgeeks.org/download-video-in-mp3-format-using-pytube/
--- ytb-downloader.py
+++ ytb-downloader.py
@@ -1,9 +1,10 @@
 from tkinter import *
 from pytube import YouTube
 from datetime import datetime
+import os
 
 window = Tk()
-window.geometry("720x400")
+window.geometry("720x480")
 window.config(bg="ROYAL BLUE")
 window.title("ytb-downloader")
 window.resizable(0,0)
@@ -12,20 +13,26 @@ window.iconphoto(False, youtube_logo)
 
 Label(window, text = " YOUTUBE VIDEO DOWNLOADER PROJECT ", font = ("Courier", 20, "bold"), bg = "WHITE").pack(padx = 5, pady = 50)
 Video_Link = StringVar()
-Label(window, text = " URL ", font = ("Roboto", 14, "bold"), bg = "WHITE").place(x= 45, y = 125)
-Entry_Link = Entry(window, width = 50, font = 24, textvariable = Video_Link).place(x = 105, y = 125)
+Mp4_path = StringVar()
+Mp3_path = StringVar()
+Label(window, text = " URL             ", font = ("Roboto", 14, "bold"), bg = "WHITE").place(x= 30, y = 175)
+Label(window, text = " MP4 PATH ", font = ("Roboto", 14, "bold"), bg = "WHITE").place(x= 30, y = 125)
+Label(window, text = " MP3 PATH ", font = ("Roboto", 14, "bold"), bg = "WHITE").place(x= 30, y = 150)
+Entry_Link = Entry(window, width = 60, font = 24, textvariable = Video_Link).place(x = 150, y = 175)
+Mp4_Link = Entry(window, width = 60, font = 24, textvariable = Mp4_path).place(x = 150, y = 125)
+Mp3_Link = Entry(window, width = 60, font = 24, textvariable = Mp3_path).place(x = 150, y = 150)
 
 # code for creating the checkbox form
 filetypevalue_mp4 = IntVar()
-filetype_mp4 = Checkbutton(text = "MP4", variable = filetypevalue_mp4).place(x = 300, y = 175)
+filetype_mp4 = Checkbutton(text = "MP4", variable = filetypevalue_mp4).place(x = 300, y = 225)
 filetypevalue_mp3 = IntVar()
-filetype_mp3 = Checkbutton(text = "MP3", variable = filetypevalue_mp3).place(x = 350, y = 175)
+filetype_mp3 = Checkbutton(text = "MP3", variable = filetypevalue_mp3).place(x = 350, y = 225)
 
 # according to the value entered in checkbox, appropriate commands are excecuted
 def getfiletype():
     if(filetypevalue_mp4.get() == 1 & filetypevalue_mp3.get() == 1):
-        video_download()
         audio_download()
+        video_download()
     else:
         if(filetypevalue_mp4.get() == 1):
             video_download()
@@ -36,10 +43,13 @@ def getfiletype():
 def video_download():
     video_url = YouTube(str(Video_Link.get()))
     video = video_url.streams.get_highest_resolution()
-    path = 'E:\Python Projects\YTB_Downloader\Downloaded Videos'
-    video.download(path)
-    Label(window, text = "MP4 Video Download Completed!", font = ("Roboto", 14, "bold"), bg = "ROYAL BLUE", fg = "WHITE").place(x = 200, y = 300)
-    Label(window, text = "CHECK YOUR PROJECT FOLDER", font = ("Roboto", 14, "bold"), bg = "ROYAL BLUE", fg = "WHITE").place(x = 200, y = 325)
+    path_mp4 = str(Mp4_path.get())
+    mp4output = video.download(path_mp4)
+    basemp4, ext = os.path.splitext(mp4output)
+    new_mp4file = basemp4 + '.mp4'
+    os.rename(mp4output, new_mp4file)
+    Label(window, text = "MP4 Video Download Completed!", font = ("Roboto", 14, "bold"), bg = "ROYAL BLUE", fg = "WHITE").place(x = 200, y = 400)
+    Label(window, text = "CHECK YOUR PROJECT FOLDER", font = ("Roboto", 14, "bold"), bg = "ROYAL BLUE", fg = "WHITE").place(x = 200, y = 425)
 
     # writing the details of the download in history.txt as a record
     with open("history.txt", "a") as f:
@@ -56,11 +66,14 @@ def video_download():
 # code for downlading the mp3 audio
 def audio_download():
     video_url = YouTube(str(Video_Link.get()))
-    audio = video_url.streams.get_audio_only()
-    path = 'E:\Python Projects\YTB_Downloader\Downloaded Audios'
-    audio.download(path)
-    Label(window, text = "MP3 Audio Download Completed!", font = ("Roboto", 14, "bold"), bg = "ROYAL BLUE", fg = "WHITE").place(x = 200, y = 300)
-    Label(window, text = "CHECK YOUR PROJECT FOLDER", font = ("Roboto", 14, "bold"), bg = "ROYAL BLUE", fg = "WHITE").place(x = 200, y = 325)
+    audio = video_url.streams.filter(only_audio=True).first()
+    path_mp3 = str(Mp3_path.get())
+    mp3output = audio.download(path_mp3)
+    basemp3, ext = os.path.splitext(mp3output)
+    new_mp3file = basemp3 + '.mp3'
+    os.rename(mp3output, new_mp3file)
+    Label(window, text = "MP3 Audio Download Completed!", font = ("Roboto", 14, "bold"), bg = "ROYAL BLUE", fg = "WHITE").place(x = 200, y = 400)
+    Label(window, text = "CHECK YOUR PROJECT FOLDER", font = ("Roboto", 14, "bold"), bg = "ROYAL BLUE", fg = "WHITE").place(x = 200, y = 425)
 
     # writing the details of the download in history.txt as a record
     with open("history.txt", "a") as f:
@@ -75,5 +88,5 @@ def audio_download():
         f.write("\n")
 
 # Download button
-Button(window, text = "DOWNLOAD", font = ("Roboto", 24, "bold"), bg = "LIGHTBLUE", command = getfiletype).place(x= 240, y = 225)
+Button(window, text = "DOWNLOAD", font = ("Roboto", 24, "bold"), bg = "LIGHTBLUE", command = getfiletype).place(x= 240, y = 275)
 window.mainloop()
